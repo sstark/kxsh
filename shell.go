020a33d1325e78623b34c2fd3ab32eb88077bb90
Sebastian Stark <stark@tuebingen.mpg.de>
2019-04-03 16:32:06 +0200

    add option for listing groups

--- shell.go
+++ shell.go
@@ -7,17 +7,37 @@ import (
 	"github.com/sstark/knxbaosip"
 	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 )
 
+type GroupMap map[string][]int
+
+func listGroups(groups GroupMap) {
+	var ell string
+	for k, v := range groups {
+		ml := len(v)
+		if ml > 8 {
+			ell = " ..."
+			ml = 8
+		} else {
+			ell = ""
+		}
+		l := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(v[:ml])), " "), "[]")
+		fmt.Printf("%12s: %s%s\n", k, l, ell)
+	}
+}
+
 func main() {
-	var cg map[string][]int
+	var cg GroupMap
 	var err error
 
 	var fGroup, fUrl, fDps string
+	var fLsGroups bool
 	flag.StringVar(&fGroup, "group", "default", "choose datapoint group")
 	flag.StringVar(&fUrl, "url", "", "specify URL of BAOS device")
 	flag.StringVar(&fDps, "dps", "dps.json", "datapoint file")
+	flag.BoolVar(&fLsGroups, "lsgroups", false, "show available groups")
 	flag.Parse()
 
 	b, err := ioutil.ReadFile(fDps)
@@ -27,6 +47,11 @@ func main() {
 	err = json.Unmarshal(b, &cg)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if fLsGroups {
+		listGroups(cg)
+		os.Exit(0)
 	}
 
 	knx := knxbaosip.NewClient(fUrl)
